c4f922d63bd07779c966f78d284947e7e204a53e
Adam <kobra.brandt@gmail.com>
2022-09-21 14:31:48 +0200

    Delete of not necessary lines of code

--- src/main/java/uni/project/weatherapp/service/WeatherService.java
+++ src/main/java/uni/project/weatherapp/service/WeatherService.java
@@ -2,7 +2,6 @@ package uni.project.weatherapp.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import uni.project.weatherapp.model.Wrapper;
 
@@ -12,9 +11,10 @@ public class WeatherService {
 
     WeatherClient weatherClient = new WeatherClient();
 
-    public Wrapper getWeatherByLocation(String location) throws HttpClientErrorException {
+    public Wrapper getWeatherByLocation(String location) {
         String url = weatherClient.getWeatherApi(location);
         RestTemplate restTemplate = new RestTemplate();
+        System.out.println(url);
         return restTemplate.getForObject(url, Wrapper.class);
     }
 }
